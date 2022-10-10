01b20f2dcb4e0a9fde4222cb7a3e45063ad60a6e
Yukha Dharmeswara <yukha.dw@samsung.com>
2022-10-10 09:22:34 +0700

    test: add test

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,4 +1,5 @@
 import _test from "ava";
+import { createData, createData2 } from "src/app/data.js";
 import { createApp } from "../src/app/app.js";
 
 _test("test1", async (t) => {
@@ -13,4 +14,11 @@ _test("test2", async (t) => {
   const app2 = createApp();
 
   t.deepEqual((app1 as any).data.b, (app2 as any).data.b);
+});
+
+_test("test3", async (t) => {
+  const data1 = createData();
+  const data2 = createData2();
+
+  t.deepEqual(data1, data2);
 });
