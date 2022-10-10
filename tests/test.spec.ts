9cede8399fa372178f6e258afbb5d7f579e80cca
Yukha Dharmeswara <yukha.dw@samsung.com>
2022-10-10 10:54:36 +0700

    test: 100%

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -14,3 +14,13 @@ _test("test2", async (t) => {
 
   t.deepEqual((app1 as any).data1.b, (app2 as any).data1.b);
 });
+
+_test("test3", async (t) => {
+  const app1 = createApp();
+  const app2 = createApp();
+
+  t.deepEqual((app1 as any).data1, (app2 as any).data12);
+  t.deepEqual((app1 as any).data2, (app2 as any).data22);
+  t.deepEqual((app1 as any).data3, (app2 as any).data32);
+  t.deepEqual((app1 as any).data4, (app2 as any).data42);
+});
